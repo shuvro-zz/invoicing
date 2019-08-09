fd2fe6cb036969e0be43bcbba30a2869d100e6f2
Denis Torresan <denis.torresan@fabvla.com>
2019-08-09 15:09:07 +0200

    compatibility with money_format on Windows

--- app/Library/functions.php
+++ app/Library/functions.php
@@ -2,6 +2,7 @@
 
 if (!function_exists("money_format")) { 
 	function money_format($format, $number){
-		return sprintf($format, $number);
+		//return sprintf($format, $number);
+		return sprintf('%01.2f', $number);
 	}
 }
